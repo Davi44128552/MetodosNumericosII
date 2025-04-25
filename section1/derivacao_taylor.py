10f554f98eff118e4e1a46042210a8e8bdf83e39
Davi44128552 <nenembrjr@gmail.com>
2025-04-25 09:24:18 -0300

    Adding derivative with cubic error

--- section1/derivacao_taylor.py
+++ section1/derivacao_taylor.py
@@ -15,5 +15,13 @@ def derivada_primeira_erro_quadratico(tipo, funcao, x, Dx):
     secante = numerador/denominador
     return secante
 
+def derivada_primeira_erro_cubico(tipo, funcao, x, Dx):
+    if (tipo == 'forward'):
+        numerador = 18*converter_funcao(funcao, x+Dx) - 9*converter_funcao(funcao, x+2*Dx) + 2*converter_funcao(funcao, x+3*Dx) - 11*converter_funcao(funcao, x)
+        denominador = 6*Dx
+
+    secante = numerador/denominador
+    return secante
+
 funcao = '2*x**3'
-print(derivada_primeira_erro_quadratico('backward', funcao, 2, 0.000001))
+print(derivada_primeira_erro_cubico('forward', funcao, 2, 0.000001))
