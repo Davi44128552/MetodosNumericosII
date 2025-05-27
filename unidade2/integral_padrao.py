f4249cfca07c92c82e9de384ad319730b3aa1374
Davi <nenembrjr@gmail.com>
2025-05-26 22:31:44 -0300

    Adding simpson's 1/3 formula

--- unidade2/integral_padrao.py
+++ unidade2/integral_padrao.py
@@ -2,8 +2,9 @@
 def funcao(x):
     return x*x
 
+# Integrais fechadas
 # Integral entre dois pontos para polinômio de primeiro grau
-def formula_trapezio(x_inicio, x_fim = None, delta_x = None):
+def formula_fechada_grau_1(x_inicio, x_fim = None, delta_x = None):
     # Se x_fim tiver sido informado, calculamos delta_x
     if (delta_x is None):
         delta_x = x_fim - x_inicio
@@ -15,13 +16,25 @@ def formula_trapezio(x_inicio, x_fim = None, delta_x = None):
     # Retornando a fórmula que calcula a integral
     return (delta_x / 2) * (funcao(x_fim) + funcao(x_inicio))
 
-def formula_simpson_grau_2(x_inicio, x_fim = None, delta_x = None):
-    return None
+def formula_fechada_grau_2(x_inicio, x_fim = None, delta_x = None):
+    # Se x_fim tiver sido informado, calculamos delta_x
+    if (delta_x is None):
+        delta_x = x_fim - x_inicio
+
+    # Se delta_x tiver sido informado, calculamos x_fim
+    elif (x_fim is None):
+        x_fim = x_inicio + delta_x
+
+    # Definindo h: Como são 3 pontos, então dividimos delta_x ao meio
+    h = delta_x / 2
+
+    # Retornando a fórmula que calcula a intergrau
+    return (delta_x / 6) * (funcao(x_fim) + 4*funcao(x_inicio + h) + funcao(x_inicio))
 
 # Funcao para calcular a integral correta
 def integral_final(x_inicio, x_fim, erro):
     erro_atual = float("inf")
-    integral_ultima = formula_trapezio(x_inicio, x_fim = x_fim)
+    integral_ultima = formula_fechada_grau_2(x_inicio, x_fim = x_fim)
     n = 1
     iteracoes = 0
     while (erro_atual > erro):
@@ -32,7 +45,7 @@ def integral_final(x_inicio, x_fim, erro):
         for i in range(n):
             a = x_inicio + i * h
             b = a + h
-            integral_atual += formula_trapezio(a, x_fim = b)
+            integral_atual += formula_fechada_grau_2(a, x_fim = b)
         erro_atual = abs(integral_atual - integral_ultima)
         integral_ultima = integral_atual
 
@@ -41,4 +54,4 @@ def integral_final(x_inicio, x_fim, erro):
 
 
 # Testando as integrais
-print(integral_final(2, 4, 0.00001))
+print(integral_final(2, 4, 0.000001))
