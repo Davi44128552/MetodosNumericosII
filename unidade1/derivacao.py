045c579f33b63d632ca834cd54a7e1a30d5d92d8
Davi <nenembrjr@gmail.com>
2025-06-02 23:46:37 -0300

    Adding more differentiations

--- unidade1/derivacao.py
+++ unidade1/derivacao.py
@@ -1,26 +1,31 @@
 def funcao(x):
     return x*x
 
+# Calculando as derivadas
+# Derivadas Primeira
 def derivada_primeira_erro_linear(tipo, x, Dx):
     if (tipo == 'forward'):
         numerador = funcao(x + Dx) - funcao(x)
-        denominador = Dx
 
     elif (tipo == 'backward'):
         numerador = funcao(x) - funcao(x - Dx)
-        denominador = Dx
 
     elif (tipo == 'central'):
         numerador = funcao(x + Dx) - funcao(x - Dx)
     
-    return numerador / denominador
+    return numerador / Dx
 
 def derivada_primeira_erro_quadratico(tipo, x, Dx):
     if (tipo == 'forward'):
         numerador = -funcao(x + 2*Dx) + 4 * funcao(x + Dx) - 3 * funcao(x)
-        denominador = 2 * Dx
 
-    return numerador / denominador
+    elif (tipo == 'backward'):
+        numerador = 3 * funcao(x) - 4 * funcao(x - Dx) + funcao(x - 2 * Dx)
+
+    elif (tipo == 'central'):
+        numerador = funcao(x + Dx) - funcao(x - Dx)
+
+    return numerador / (2 * Dx)
 
 def derivada_primeira_erro_cubico(tipo, x, Dx):
     if (tipo == 'forward'):
@@ -29,6 +34,19 @@ def derivada_primeira_erro_cubico(tipo, x, Dx):
 
     return numerador / denominador
 
+# Derivadas Segunda
+def derivada_segunda_erro_linear(tipo, x, Dx):
+    if (tipo == 'forward'):
+        numerador = funcao(x + 2 * Dx) - 2 * funcao(x + Dx) + funcao(x)
+
+    elif (tipo == 'backward'):
+        numerador = funcao(x) - 2 * funcao(x - Dx) + funcao(x - 2 * Dx)
+
+    elif (tipo == 'central'):
+        numerador = funcao(x + Dx) - 2 * funcao(x) + funcao(x - Dx)
+        
+    return numerador / (Dx * Dx)
+
 print(derivada_primeira_erro_linear('forward', 2, 0.00001))
-print(derivada_primeira_erro_quadratico('forward', 2, 0.00001))
+print(derivada_primeira_erro_quadratico('central', 2, 0.00001))
 print(derivada_primeira_erro_cubico('forward', 2, 0.00001))
